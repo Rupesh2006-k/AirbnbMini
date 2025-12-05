0c0a82d0060222af95e1612d2a96a23ed7c5f02c
Rupesh kushwaha <rupeshkushwaha03032@gmail.com>
2025-12-05 07:52:02 +0530

    update

--- config/cloud.config.js
+++ config/cloud.config.js
@@ -1,5 +1,28 @@
+// /** @format */
+// const cloudinary = require("cloudinary").v2;
+// const { CloudinaryStorage } = require("multer-storage-cloudinary");
+// const multer = require("multer");
+
+// cloudinary.config({
+//   cloud_name: process.env.CLOUD_NAME,
+//   api_key: process.env.API_KEY,
+//   api_secret: process.env.API_SECRET,
+// });
+
+// const storage = new CloudinaryStorage({
+//   cloudinary,
+//   params: {
+//     folder: "MiniAirBnb",
+//     allowed_formats: ["jpeg", "png", "jpg"],
+//   },
+// });
+
+// const upload = multer({ storage });
+
+// module.exports = { cloudinary, storage, upload };
+
 /** @format */
-const cloudinary = require("cloudinary").v2;
+const { v2: cloudinary } = require("cloudinary");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const multer = require("multer");
 
@@ -10,10 +33,14 @@ cloudinary.config({
 });
 
 const storage = new CloudinaryStorage({
-  cloudinary,
-  params: {
-    folder: "MiniAirBnb",
-    allowed_formats: ["jpeg", "png", "jpg"],
+  cloudinary: cloudinary,
+  params: async (req, file) => {
+    return {
+      folder: "MiniAirBnb",
+      allowed_formats: ["jpeg", "png", "jpg"],
+      resource_type: "image",
+      public_id: `${Date.now()}-${file.originalname}`,
+    };
   },
 });
 
